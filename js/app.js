b9e23af445d3a965084aecabfc3bf945228e116c
Cielito <cielitomcantero@gmail.com>
2023-09-16 18:41:55 +0800

    Add recent products section

--- js/app.js
+++ js/app.js
@@ -41,6 +41,47 @@ const categories = [
     }
 ];
 
+const products = [
+    {
+        'id': 1,
+        'title': 'Festive Looks Rust Red Ribbed Velvet Long Sleeve Bodysuit',
+        'image': 'images/product/product-1.jpg',
+        'link': '#',
+        'price': '$38'
+    },
+    {
+        'id': 2,
+        'title': 'Chevron Flap Crossbody Bag',
+        'image': 'images/product/product-2.jpg',
+        'link': '#',
+        'price': '$7.34',
+        'sale_price': '$5.77'
+    },
+    {
+        'id': 3,
+        'title': 'Manilla Tan Multi Plaid Oversized Fringe Scarf',
+        'image': 'images/product/product-3.jpg',
+        'link': '#',
+        'price': '$39',
+        'sale_price': '$29'
+    },
+    {
+        'id': 4,
+        'title': 'Diamante Puff Sleeve Dress - Black',
+        'image': 'images/product/product-4.jpg',
+        'link': '#',
+        'price': '$45.99'
+    },
+    {
+        'id': 5,
+        'title': 'Banneth Open Front Formal Dress in Black',
+        'image': 'images/product/product-5.jpg',
+        'link': '#',
+        'price': '$99.95',
+        'sale_price': '$69'
+    }
+];
+
 const initApp = () => {
     // Slider
     const sliderEl = document.getElementById('home-slider');
@@ -75,6 +116,36 @@ const initApp = () => {
         });
 
         categoriesEl.innerHTML = categoriesHtml;
+    }
+
+    // Products
+    const productsEl = document.querySelector('.container-recent-products .list-products');
+    if (products?.length && productsEl) {
+        let productsHtml = '';
+        products.forEach((product) => {
+            let priceHtml = '';
+            if (product.sale_price) {
+                priceHtml = `
+                    <span class="sale-price">${product.sale_price}</span>
+                    <span class="price"><s>${product.price}</s></span>
+                `;
+            } else {
+                priceHtml = `<span class="price">${product.price}</span>`;
+            }
+            productsHtml += `
+                <div class="product" data-id="${product.id}">
+                    <div class="product-image">
+                        <img src="${product.image}">
+                    </div>
+                    <a class="product-title" href="${product.link}">${product.title}</a>
+                    <div class="product-price">
+                        ${priceHtml}
+                    </div>
+                </div>
+            `;
+        });
+
+        productsEl.innerHTML = productsHtml;
     }
 }
 
